b09803f94a7caad2dffd5b5d7a28bfa622f4e0bb
Evan Harrigan <harriganevan@gmail.com>
2023-12-21 15:24:35 -0500

    first commit

---
+++ portfolio/main.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+  //options here
+  licenseKey: 'gplv3-license',
+  // responsiveWidth: 768,
+  autoScrolling: true,
+  navigation: true,
+  navigationTooltips: ['Home', 'About', 'Projects', 'Skills', 'Contact'],
+  anchors: ['home', 'about', 'projects', 'skills', 'contact'],
+  slidesNavigation: true,
+  showActiveTooltip: false,
+  scrollOverflow: false
+});
